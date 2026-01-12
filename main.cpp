d8c8dafd6544a4ce2b39503214afa1ac8110ad84
rkc137 <hlopov100@gmail.com>
2026-01-12 18:26:58 +0300

    some updates and fixes

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #include <cstring>
 #include <thread>
 #include <chrono>
-#include <unistd.h>
+#include <set>
 #include <stdlib.h>
 #include <filesystem>
+#include <windows.h>
 
 #include <SFML/Audio.hpp>
 
@@ -12,122 +13,154 @@
 
 namespace fs = std::filesystem;
 
-std::string snow(int s)
-{
-    std::string line(s, ' ');
-    for(char &c : line)
-        if(rand() % 30 == 0)
-            c = '.';
-    return line;
-}
-
-std::string trees_toys(int x, int y, int frame)
-{
-    std::string toy;
-    if(x % 4 == 0)
-    {
-        if(y % 2 == frame % 2)
-            toy += clr::on_red;
-        else
-            toy += clr::on_cyan;
-        toy += '&' + clr::white;
-    }
-    else
-        toy += clr::green + '*' + clr::white;
-    return toy;
-}
-
 int main(int argc, char *argv[])
 {
     int tall = 10;
-    bool is_ingore_mp3 = false;
-    bool is_ingore_other_files = false;
-    std::string music_folder_name = "music";
+    bool without_music = false;
+    const std::set<std::string_view> available_exts = {".flac", ".wav", ".ogg", ".mp3"};
+    fs::path music_folder = {"./music"};
+    std::vector<std::string> music_names;
+
+
     for(int i = 1; i < argc; i++)
     {
-        if(!is_ingore_mp3) is_ingore_mp3 = !strcmp(argv[i], "-ignmp3");
-        if(!is_ingore_other_files) is_ingore_other_files = !strcmp(argv[i], "-ign");
-        
-        if(i + 1 != argc)
+        std::string_view arg = argv[i];
+        bool uknown_flag = false;
+
+        if(arg == "-nm" || arg == "-nomusic")
+            without_music = true;
+        else if(i + 1 != argc)
         {
-            if(!strcmp(argv[i], "-f"))              
-                music_folder_name = argv[i + 1];
-            if(!strcmp(argv[i], "-t"))
-                tall = atoi(argv[i + 1]);
             i++;
-            continue;
+            std::string_view arg_value = argv[i];
+
+            if(arg == "-f" || arg == "--folder")
+            {
+                music_folder = arg_value;
+                if(!fs::exists(music_folder) || !fs::is_directory(music_folder))
+                {
+                    std::cerr << music_folder << " did not exist or not a folder\n";
+                    return EXIT_FAILURE;
+                }
+            }
+            else if(arg == "-t" || arg == "--tall")
+            {
+                if(auto result = std::from_chars(arg_value.data(), arg_value.data() + arg_value.size(), tall);
+                    result.ec == std::errc::invalid_argument)
+                {
+                    std::cerr << "bad tall size\n";
+                    return EXIT_FAILURE;
+                }
+            }
+            else
+                uknown_flag = true;
+            i -= uknown_flag;
         }
+        else
+            uknown_flag = true;
+
+        if(uknown_flag)
+            std::cout << "unknown flag \"" << arg << "\", ignored\n";
     }
-    
-    if(music_folder_name.back() == '/')
-        music_folder_name.pop_back();
-    
-    const std::set<std::string> available_exts = {".flac", ".wav", ".ogg"};
-    std::set<std::string> music_names;
-    for(auto &p: fs::recursive_directory_iterator(music_folder_name))
+
+    if(!without_music)
     {
-        std::string ext(p.path().extension().string()),
-               str_path(p.path().string()),
-              file_name(str_path.substr(music_folder_name.size() + 1, str_path.size()));
-        
-        if(fs::is_directory(p))
-            continue;
-        if(ext == ".mp3")
+        if(!fs::exists(music_folder) )
         {
-            if(is_ingore_mp3 || is_ingore_other_files)
-                continue;
-            std::cout << "mp3 is not working, so you need to use -ign to ignore all not available\n"
-                      << "files or -ignmp3 to ignore mp3 files in folder\n" 
-                      << "and ofcse you can just format " 
-                      << file_name << " to some of there:\n";
-            for(auto ext : available_exts)
-                std::cout << ext << '\n';
-            return 1;
+            std::cerr << music_folder << " folder did not exist";
+            return EXIT_FAILURE;
         }
-        if(available_exts.find(ext) == available_exts.end())
+        else for(const auto &p : fs::recursive_directory_iterator(music_folder))
         {
-            if(is_ingore_other_files)
+            if(!fs::is_regular_file(p))
                 continue;
-            std::cout << file_name << " is wrong format\nyou can use -ign to ignore all not available files\n";
-            return 1;
+            if(const auto &path = p.path(); available_exts.contains(path.extension().string()))
+                music_names.push_back(path.filename().string());
         }
-        if(fs::is_regular_file(p))
-            music_names.insert(file_name);
     }
 
     for(auto &name : music_names)
         std::cout << name << '\n';
-    std::cout << "\nwere found in " << music_folder_name << "\nctrl + c to exit\n";
+    std::cout << "\nwere found in " << music_folder.filename() << "\nctrl + c to exit\n";
     std::this_thread::sleep_for(std::chrono::seconds(4));
 
 
     int w, h;
     int offtop_x, offtop_y;
-    
+
     sf::Music music;
     auto music_iterator = music_names.end();
+    fs::path music_path;
     std::string output;
     for(int frame = 0; true; frame++)
     {
-        if(music.getStatus() != sf::Music::Status::Playing)
+        if(!without_music && music.getStatus() != sf::Music::Status::Playing)
         {
             if(music_iterator != music_names.end())
                 music_iterator++;
             else
                 music_iterator = music_names.begin();
 
-            music.openFromFile(music_folder_name + '/' + *music_iterator);
-            music.play();
+            music_path = music_folder / *music_iterator;
+            if(music.openFromFile(music_path))
+                music.play();
+            else
+                std::cerr << "unable to open: " << *music_iterator;
+        }
+        output.clear();
+
+        {
+            static auto console = std::invoke([&]{
+                struct {
+                    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+                    CONSOLE_SCREEN_BUFFER_INFO Info;
+                } console;
+                GetConsoleScreenBufferInfo(console.handle, &console.Info);
+                return console;
+            });
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+            GetConsoleScreenBufferInfo(console.handle, &csbi);
+            DWORD written;
+            COORD topLeft = {0, 0};
+            DWORD size = csbi.dwSize.X * csbi.dwSize.Y;
+            FillConsoleOutputCharacter(console.handle, ' ', size, topLeft, &written);
+            FillConsoleOutputAttribute(console.handle, csbi.wAttributes, size, topLeft, &written);
+
+            static auto setCursorPosition = [&](short x, short y){
+                COORD coord = {x, y};
+                SetConsoleCursorPosition(console.handle, coord);
+            };
+            setCursorPosition(0, 0);
+            w = console.Info.srWindow.Right - console.Info.srWindow.Left + 1;
+            h = console.Info.srWindow.Bottom - console.Info.srWindow.Top + 10;
         }
 
-        if(frame % 10 == 0)
-            std::system("clear");
-        
-        w = WEXITSTATUS(std::system("exit `tput cols`"));
-        h = WEXITSTATUS(std::system("exit `tput lines`")) + 7;
-      
+
         offtop_x = w / 2 - tall;
         offtop_y = h * 3 / 4 - tall;
+
+        static auto snow = [](int s){
+            std::string line(s, ' ');
+            for(char &c : line)
+                if(rand() % 30 == 0)
+                    c = '.';
+            return line;
+        };
+
+        static auto toy = [&frame](int x, int y){
+            std::string toy;
+            if(x % 4 == 0)
+            {
+                if(y % 2 == frame % 2)
+                    toy += clr::on_red;
+                else
+                    toy += clr::on_cyan;
+                toy += '&';
+            }
+            else
+                toy += clr::green + '*';
+            return toy + clr::white;
+        };
 
         for(int y = 0; y < offtop_y; y++)
             output += snow(w) + '\n';
@@ -143,15 +176,18 @@ int main(int argc, char *argv[])
                     if(rand() % (30 + tall) > clr::toys_clrs.size())
                         output += clr::green + '*' + clr::white;
                     else
-                        output += *clr::toys_clrs[rand() % clr::toys_clrs.size()] + '&' + clr::white;
+                    {
+                        output += clr::toys_clrs[rand() % clr::toys_clrs.size()];
+                        output += '&' + clr::white;
+                    }
                 }
             }
             else if(frame % 20 > 10)
                 for(int x = y * 2 + 1; x > 0; x--)
-                    output += trees_toys(x, y, frame);
+                    output += toy(x, y);
             else
                 for(int x = 0; x < y * 2 + 1; x++)
-                    output += trees_toys(x, y, frame);
+                    output += toy(x, y);
             output += clr::white + '\\' + snow(tall - y + offtop_x - 3) + '\n';
         }
 
@@ -159,14 +195,14 @@ int main(int argc, char *argv[])
         std::string under_tree(tall + 1, ' ');
         output += under_tree;
         output += clr::white + clr::underline + clr::brown + '#' + clr::white + clr::underline;
-        output += under_tree + snow(offtop_x - 3);
+        output += under_tree + snow(offtop_x - 3) + '\n';
 
-        int file_name_start = music_iterator->find_last_of('/') + 1;
-        output += '\n' + clr::white 
-                + music_iterator->substr(file_name_start, music_iterator->find_last_of('.') - file_name_start)
-                + '\n';
-        std::cout << output;
+        output += clr::white;
+        if(!without_music)
+            output += music_path.stem().string();
+
+        std::cout << output << '\n';
         std::this_thread::sleep_for(std::chrono::seconds(2));
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
